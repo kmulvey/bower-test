05cac1e61affa61a00ec5f9e9c1ae9239230ae6b
Kevin Mulvey <king.feruke@gmail.com>
2014-03-16 15:59:40 -0400

    init

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-bower-task');
+
+	grunt.initConfig({
+	  bower: {
+	    install: {
+	      options: {
+	        cleanBowerDir: true,
+					layout: 'byType',
+					targetDir: 'dist/',
+					verbose: true
+	      }
+	    }
+	  }
+	});
+
+}
